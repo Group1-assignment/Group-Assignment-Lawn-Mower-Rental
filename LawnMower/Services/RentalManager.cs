3b525e7ad6a1f5516787da54b653755770a65b83
Thord Möller <thord_moller_92@hotmail.com>
2023-10-21 09:23:16 +0200

    modified Snehas registrCustomer method to fit in the structure

--- LawnMower/Services/RentalManager.cs
+++ LawnMower/Services/RentalManager.cs
@@ -8,11 +8,28 @@ namespace LawnMowerRentalAssignment {
     public class RentalManager {
         private List<Customer> customers = new List<Customer>();
 
-        public void RegisterNewCustomer(Customer customer) {
-            customers.Add(customer);
+
+        public void RegisterCustomer(string name, int phoneNumber, CustomerType customerType) {
+            if(PhoneNumberExists(phoneNumber)) {
+                Console.WriteLine("Customer with phone number: " + phoneNumber + " is already registered");
+            }
+            else {
+                Customer customer = new Customer(name, phoneNumber, customerType);
+
+                customers.Add(customer);
+                Console.WriteLine("Registered customer: \n" + customer.ToString());
+            }
         }
 
+        public Customer? GetCustomerByPhoneNumber(int phoneNumber) {
+            Customer ?customer = customers.FirstOrDefault(customer => customer.PhoneNumber == phoneNumber);
 
+            return customer;
+        }
+
+        public bool PhoneNumberExists (int phoneNumber) {
+            return GetCustomerByPhoneNumber(phoneNumber) != null;
+        }
     }
 
 
