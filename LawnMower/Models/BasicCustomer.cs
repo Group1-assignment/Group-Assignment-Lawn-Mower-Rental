9ce3e60c42082334d868be270b2d2aafe55d3177
Thord Möller <thord_moller_92@hotmail.com>
2023-10-25 16:23:41 +0200

    Added Bledons Customer classes

---
+++ LawnMower/Models/BasicCustomer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMowerRentalAssignment
+{
+    class BasicCustomer:Customer
+    {
+        public BasicCustomer(string name, int phoneNumber, CustomerType customerType) : base(name, phoneNumber, customerType) {
+        }
+        public int MinDays { get; set; }
+        // minimum  7 days betalning men kund får lämna tillbaka när som 
+
+        public void PricePlan1(Rental rentalObject, int minDays = 7) {
+            int actualDays = rentalObject.DaysPassed();
+            if(actualDays < 7) {
+                Console.WriteLine("Basic Coustumers are charged for at least 7 days");
+                rentalObject.AdjustDays(minDays);
+            }
+
+            Rentals.Add(rentalObject);
+        }
+
+
+
+        // 25 % discount 
+        public decimal Offer { get; set; }
+        public DateTime EndDate { get; set; }
+
+        bool coupon = false;
+
+        decimal Discount(decimal offer, DateTime endDate) {
+            Offer = offer;
+            EndDate = endDate; // year.12,31
+            coupon = false;
+
+            // hur man håller koll på per år 
+            // bool = true => enddate
+            // overloading
+            return 0;
+        }
+
+        bool EndOffer() {
+            if(coupon = false) {
+                DateTime date = DateTime.Now.Date;
+                DateTime endDate = new DateTime(date.Year, 12, 31);
+
+
+                return date > endDate;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ LawnMower/Models/PrimeCustomer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMowerRentalAssignment.Models
+{
+    class PrimeCustomer:Customer
+    {
+        public decimal PointsPerDay { get; set; } // bonus p 
+
+        public PrimeCustomer(string name, int phoneNumber, CustomerType customerType) : base(name, phoneNumber, customerType) { }
+
+
+        // går att använda samma typ som pris
+        decimal Points() {
+
+
+            return PointsPerDay;
+        }
+    }
+}
